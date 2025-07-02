9115e250577b6c609966949ebdaf74159b6b9af7
Shruti Algam <139902913+shrutisep@users.noreply.github.com>
2025-07-02 11:32:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame,sys
+from grid import Grid
+pygame.init()
+dark_blue=(44,44,127)
+
+screen=pygame.display.set_mode((300,600))
+pygame.display.set_caption("pytetris")
+
+
+clock =pygame.time.Clock()
+
+game_grid=Grid()
+game_grid.print_grid()
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    screen.fill(dark_blue)
+    pygame.display.update()
+    clock.tick(60)
+
+    
+
+
